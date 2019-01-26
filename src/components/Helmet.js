4243dc39a44f72e1c9413e8514208c1707f6b8b2
Daniel Cousino <daniel.cousino27@gmail.com>
2019-01-26 11:41:06 -0500

    fixed prop types issue

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactHelmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 import { primary } from '../../colors';
+import { withTheme } from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Helmet = ({ theme = {} }) => (
   <StaticQuery
